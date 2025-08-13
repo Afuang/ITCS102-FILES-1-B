464a77dc81841f4329a9031e832713f283552d75
Afuang <adrianneilda@gmail.com>
2025-08-13 22:25:01 +0800

    Add files via upload

--- Code_Challenge1.py
+++ Code_Challenge1.py
@@ -1,1 +1,3 @@
-print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\tHi\t\t\t\t*\n\t\t\t*\t\t\t\tAfuang\t\t\t\t*\n\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t*")
+name = input("What is your name?")
+
+print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\tHi\t\t\t\t*\n\t\t\t*\t\t\t\t",name,"\t\t\t\t*\n\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t*")
