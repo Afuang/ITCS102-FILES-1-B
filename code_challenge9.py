1f89402e29a7abd05cf5d927c6b03524189fb25d
Afuang <adrianneilda@gmail.com>
2025-09-17 11:19:28 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,8 @@
+print("🦜PARROT SIMULATOR")
+speak = input("What do you want your parrot to say? ")
+rep = eval(input("How many times should the parrot squawk it? "))
+
+print("Listen to your parrot:")
+
+for i in range(1, rep, 1):
+    print("🦜 Squawk!",speak)
