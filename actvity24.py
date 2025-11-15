ee7c47dbd6be6253b81c3e71eee7debb21082a93
Afuang <adrianneilda@gmail.com>
2025-11-15 13:41:41 +0800

    Add files via upload

---
+++ actvity24.py
@@ -0,0 +1,9 @@
+from activity24_1.py import Greetpersonname, Greetinfo
+
+def Greetperson():
+    print("Hi Visitor, Welcome to my domain")
+    print("please feel free to browse around")
+    
+    Greetperson()
+    Greetpersonname('Adrian Neil D. Afuang')
+    Greetinfo('Troy','Mayao','22')
