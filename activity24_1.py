b276b6308c5c00ca5b5ef22698e915233cb21e4b
Afuang <adrianneilda@gmail.com>
2025-11-15 14:05:53 +0800

    Add files via upload

---
+++ activity24_1.py
@@ -0,0 +1,5 @@
+def Greetpersonname(name):
+    print("My Name is", name, "im the owner of this domain")
+    
+def Greetinfo(name, location, age):
+    print('Hi my Name is',name,'from',location,'im',age,'years old')
