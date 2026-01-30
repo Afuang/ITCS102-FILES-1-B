24a3978d9bed1e66d86e0b9721fb0a3fe3cb8730
Afuang <adrianneilda@gmail.com>
2026-01-30 19:08:04 +0800

    Add files via upload

--- AFUANG_HO1.py
+++ AFUANG_HO1.py
@@ -1,18 +1,26 @@
-word = input("Enter a word:")
-word_len = len(word)
-number_list = [ ]
-total = 0
-for i in range(1,1+word_len,1):
-    number = eval(input(f"Enter number {i}:"))
-    total += number
-    number_list.append(number)
-average = total / i
-print(number_list)
-print(f"The length of the word is {i}")
-print(f"The average of the numbers is {average}")
-if average == i:
-    print(f"The length of the word '{word}' is equal the average.")
-elif average >= i:
-    print(f"The length of the word '{word}' is greater than the average.")
-elif average <= i:
-    print(f"The length of the word '{word}' is less than the average.")
+def word_number_average():
+    word = input("Enter a word:")
+    word_len = len(word)
+
+    number_list = []
+    total = 0
+
+    for i in range(1, word_len + 1):
+        number = int(input(f"Enter number {i}:"))
+        total += number
+        number_list.append(number)
+
+    average = total / word_len
+
+    print(number_list)
+    print(f"The length of the word is {word_len}")
+    print(f"The average of the numbers is {average}")
+
+    if average == i:
+        print(f"The length of the word '{word}' is equal to the average.")
+    elif average <= i:
+        print(f"The length of the word '{word}' is less than the average.")
+    elif average >= i:
+        print(f"The length of the word '{word}' is greater than the average.")
+
+word_number_average()
